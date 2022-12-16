1caf27be6ec64ed10ff046258d9717298936013a
Nirintsoa <hhery889@gmail.com>
2022-12-16 22:26:50 +0300

    feat: improve error message

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 export default class IOCContainer {
-    private dependencies = new Map()
+  private dependencies = new Map();
 
-    set(id: any, dep: any): this {
-        this.dependencies.set(id, dep)
-        return this
-    }
+  set(id: any, dep: any): this {
+    this.dependencies.set(id, dep);
+    return this;
+  }
 
-    get<T>(id: any): T {
-        if (this.dependencies.has(id) === false) throw `Invalid dependency`
-        return this.dependencies.get(id)
-    }
+  get<T>(id: any): T {
+    if (this.dependencies.has(id) === false)
+      throw `"${id}" dependency not found`;
+    return this.dependencies.get(id);
+  }
 }
